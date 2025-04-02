fb9f8504204cdfc9b763c4bf2caf2896a543583a
bruno-sl15 <bruno.silvaleite15@gmail.com>
2025-04-02 20:32:51 -0300

    eventos de click na cor do fone

---
+++ script.js
@@ -0,0 +1,13 @@
+// ...existing code...
+
+document.getElementById('fone-1').addEventListener('click', () => {
+    document.getElementById('fone-destaque').setAttribute('src', 'img/fone_destaque_1.png');
+});
+
+document.getElementById('fone-2').addEventListener('click', () => {
+    document.getElementById('fone-destaque').setAttribute('src', 'img/fone_destaque_2.png');
+});
+
+document.getElementById('fone-3').addEventListener('click', () => {
+    document.getElementById('fone-destaque').setAttribute('src', 'img/fone_destaque_3.png');
+});
